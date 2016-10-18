418b5b60449aaca7e79a271251564af73a2008bb
hyagosouzza <hyagosouzza@hotmail.com>
2016-10-17 22:59:27 -0200

    Arrumando espaçamento

--- Exercicios/src/test/java/com/github/hyagosouzza/cs20162aula02/exercicios/PrimoTest.java
+++ Exercicios/src/test/java/com/github/hyagosouzza/cs20162aula02/exercicios/PrimoTest.java
@@ -1,7 +1,11 @@
 package com.github.hyagosouzza.cs20162aula02.exercicios;
+
 import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class PrimoTest {
+    
     @Test
     public void testPrimo() {
         int n = 1;
@@ -10,6 +14,7 @@ public class PrimoTest {
         boolean result = instance.primo(n);
         assertEquals(expResult, result);
     }
+    
     @Test
     public void testPrimo2() {
         int n = 11;
@@ -18,6 +23,7 @@ public class PrimoTest {
         boolean result = instance.primo(n);
         assertEquals(expResult, result);
     }
+    
     @Test
     public void testPrimo3() {
         int n = 15;
@@ -26,6 +32,7 @@ public class PrimoTest {
         boolean result = instance.primo(n);
         assertEquals(expResult, result);
     }
+    
     @Test
     public void testPrimo4() {
         int n = 9;
@@ -34,6 +41,7 @@ public class PrimoTest {
         boolean result = instance.primo(n);
         assertEquals(expResult, result);
     }
+    
     @Test
     public void testPrimo5() {
         int n = 5;
@@ -42,6 +50,7 @@ public class PrimoTest {
         boolean result = instance.primo(n);
         assertEquals(expResult, result);
     }
+    
     @Test
     public void testPrimo6() {
         int n = 10;
@@ -50,6 +59,7 @@ public class PrimoTest {
         boolean result = instance.primo(n);
         assertEquals(expResult, result);
     }
+    
     @Test
     public void testPrimo7() {
         int n = 3;
@@ -58,6 +68,7 @@ public class PrimoTest {
         boolean result = instance.primo(n);
         assertEquals(expResult, result);
     }
+
     @Test
     public void testPrimo8() {
         int n = 2;
@@ -65,5 +76,5 @@ public class PrimoTest {
         Primo instance = new Primo();
         boolean result = instance.primo(n);
         assertEquals(expResult, result);
-    } 
+    }
 }
