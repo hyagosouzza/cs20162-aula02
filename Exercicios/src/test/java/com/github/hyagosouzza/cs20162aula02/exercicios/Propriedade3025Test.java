e2630ddffe35c9a91f4e1615a4231ffc7714b2c6
hyagosouzza <hyagosouzza@hotmail.com>
2016-10-17 23:02:13 -0200

    Arrumando espaçamento

--- Exercicios/src/test/java/com/github/hyagosouzza/cs20162aula02/exercicios/Propriedade3025Test.java
+++ Exercicios/src/test/java/com/github/hyagosouzza/cs20162aula02/exercicios/Propriedade3025Test.java
@@ -1,7 +1,11 @@
 package com.github.hyagosouzza.cs20162aula02.exercicios;
+
 import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class Propriedade3025Test {
+    
     @Test
     public void testPropriedade() {
         int n = 2;
@@ -10,6 +14,7 @@ public class Propriedade3025Test {
         boolean result = instance.propriedade(n);
         assertEquals(expResult, result);
     }
+    
     @Test
     public void testPropriedade2() {
         int n = 0;
@@ -18,6 +23,7 @@ public class Propriedade3025Test {
         boolean result = instance.propriedade(n);
         assertEquals(expResult, result);
     }
+    
     @Test
     public void testPropriedade3() {
         int n = 1;
@@ -26,6 +32,7 @@ public class Propriedade3025Test {
         boolean result = instance.propriedade(n);
         assertEquals(expResult, result);
     }
+    
     @Test
     public void testPropriedade4() {
         int n = 3025;
@@ -34,6 +41,7 @@ public class Propriedade3025Test {
         boolean result = instance.propriedade(n);
         assertEquals(expResult, result);
     }
+    
     @Test
     public void testPropriedade5() {
         int n = 10;
@@ -42,6 +50,7 @@ public class Propriedade3025Test {
         boolean result = instance.propriedade(n);
         assertEquals(expResult, result);
     }
+    
     @Test
     public void testPropriedade6() {
         int n = 25;
