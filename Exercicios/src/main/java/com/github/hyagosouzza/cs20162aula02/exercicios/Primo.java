5b4519d7fb249157e437070d82df1b302f2089a9
hyagosouzza <hyagosouzza@hotmail.com>
2016-10-19 11:22:11 -0200

    Arrumando espaçamento

--- Exercicios/src/main/java/com/github/hyagosouzza/cs20162aula02/exercicios/Primo.java
+++ Exercicios/src/main/java/com/github/hyagosouzza/cs20162aula02/exercicios/Primo.java
@@ -2,15 +2,17 @@
  * Copyright (c) 2016. Fábrica de Software - Instituto de Informática (UFG)
  * Creative Commons Attribution 4.0 International License.
  */
+
 package com.github.hyagosouzza.cs20162aula02.exercicios;
-/**Classe para utilizar o método primo.
- * @author Hyago Vieira de Souza
+
+/**
+ * Classe para utilizar o método primo.
  * @version 1.0
  */
 public class Primo {
-     /**Metodo para verificar se o número é primo.
-     * @author Hyago Vieira de Souza
-     * @since Release 01 da aplicação
+     
+     /**
+     * Metodo para verificar se o número é primo.
      * @param n final int
      * @return boolean - j
      */
