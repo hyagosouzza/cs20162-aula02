295ec43fa9bc17a919823fda71d806d8bdc8b0e9
hyagosouzza <hyagosouzza@hotmail.com>
2016-10-19 11:21:10 -0200

    Arrumando espaçamento

--- Exercicios/src/main/java/com/github/hyagosouzza/cs20162aula02/exercicios/PotenciaSoma.java
+++ Exercicios/src/main/java/com/github/hyagosouzza/cs20162aula02/exercicios/PotenciaSoma.java
@@ -2,15 +2,17 @@
  * Copyright (c) 2016. Fábrica de Software - Instituto de Informática (UFG)
  * Creative Commons Attribution 4.0 International License.
  */
+
 package com.github.hyagosouzza.cs20162aula02.exercicios;
-/**Classe para utilizar o método potenciaSoma.
- * @author Hyago Vieira de Souza
+
+/**
+ * Classe para utilizar o método potenciaSoma.
  * @version 1.0
  */
 public class PotenciaSoma {
-    /**Metodo para buscar o resultado de "x" elevado a "y" (somando).
-     * @author Hyago Vieira de Souza
-     * @since Release 01 da aplicação
+    
+    /**
+     * Metodo para buscar o resultado de "x" elevado a "y" (somando).
      * @param x final int
      * @param y final int
      * @return int - potencia - resultado da potenciação
