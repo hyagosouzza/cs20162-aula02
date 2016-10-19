bd8f51d6bd1cd705bb8219576784b92ce6754ade
hyagosouzza <hyagosouzza@hotmail.com>
2016-10-19 11:26:09 -0200

    Arrumando espaçamento

--- Exercicios/src/main/java/com/github/hyagosouzza/cs20162aula02/exercicios/SomaNaturais.java
+++ Exercicios/src/main/java/com/github/hyagosouzza/cs20162aula02/exercicios/SomaNaturais.java
@@ -2,15 +2,17 @@
  * Copyright (c) 2016. Fábrica de Software - Instituto de Informática (UFG)
  * Creative Commons Attribution 4.0 International License.
  */
+
 package com.github.hyagosouzza.cs20162aula02.exercicios;
-/**Classe para utilizar o método soma.
- * @author Hyago Vieira de Souza
+
+/**
+ * Classe para utilizar o método soma.
  * @version 1.0
  */
 public class SomaNaturais {
-    /**Metodo somar os "n" primeiros números naturais.
-     * @author Hyago Vieira de Souza
-     * @since Release 01 da aplicação
+    
+    /**
+     * Metodo somar os "n" primeiros números naturais.
      * @param n final int
      * @return int - s - soma dos naturais
      */
