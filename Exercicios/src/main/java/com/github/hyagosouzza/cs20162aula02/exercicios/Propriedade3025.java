60a1c8a1071794f673b4b44a61a2c4fe23dcd39e
hyagosouzza <hyagosouzza@hotmail.com>
2016-10-19 11:25:06 -0200

    Arrumando espaçamento

--- Exercicios/src/main/java/com/github/hyagosouzza/cs20162aula02/exercicios/Propriedade3025.java
+++ Exercicios/src/main/java/com/github/hyagosouzza/cs20162aula02/exercicios/Propriedade3025.java
@@ -2,16 +2,17 @@
  * Copyright (c) 2016. Fábrica de Software - Instituto de Informática (UFG)
  * Creative Commons Attribution 4.0 International License.
  */
+
 package com.github.hyagosouzza.cs20162aula02.exercicios;
 
-/**Classe para utilizar o método a propriedade3025.
- * @author Hyago Vieira de Souza
+/**
+ * Classe para utilizar o método a propriedade3025.
  * @version 1.0
  */
 public class Propriedade3025 {
-    /**Metodo para verificar se o número recebido tem a "propriedade 3025".
-     * @author Hyago Vieira de Souza
-     * @since Release 01 da aplicação
+    
+    /**
+     * Metodo para verificar se o número recebido tem a "propriedade 3025".
      * @param n final int
      * @return boolean
      */
