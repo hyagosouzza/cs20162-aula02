869efba4322722359c3f9d48b87af29628640f6f
hyagosouzza <hyagosouzza@hotmail.com>
2016-10-19 11:30:09 -0200

    Descrição do pacote

--- Exercicios/src/main/java/com/github/hyagosouzza/cs20162aula02/exercicios/package-info.java
+++ Exercicios/src/main/java/com/github/hyagosouzza/cs20162aula02/exercicios/package-info.java
@@ -1,6 +1,6 @@
 /**
  * Pacote : com.github.hyagosouzza.cs20162aula02.exercicios.
- * @author Hyago Vieira de Souza
+ * Pacote que contém os métodos/algoritmos da primeira lista de exercício.
  * @version 1.1
  */
 package com.github.hyagosouzza.cs20162aula02.exercicios;
